7ed945d13bc6ccfac1e882fb13af25ab886d4a94
Chelsea Youmans <chelsea@oochelz.com>
2012-08-05 19:58:09 -0400

    Pushing what I've got for mailing.

--- Projexi/Projexi/Controllers/ProjectSubmissionController.cs
+++ Projexi/Projexi/Controllers/ProjectSubmissionController.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 using Projexi.Models;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using SendGridMail;
+using SendGridMail.Transport;
 
 namespace Projexi.Controllers {
     public class ProjectSubmissionController : Controller {
@@ -18,7 +24,18 @@ namespace Projexi.Controllers {
         // POST: /ProjectSubmission/
         [HttpPost]
         public ActionResult Index(ProjectSubmission project) {
-            // TODO Send an email with the project in it
+            string username = "ba04776e-78bc-479a-a2a0-8035d31e119a";
+            string password = "9o<turtifiC>>p";
+
+            ISendGrid email = SendGrid.GenerateInstance();
+            email.AddTo("chelsea@oochelz.com");
+            email.From = new MailAddress("blahh@nshdf.com");
+            email.Subject = "Yay, mail!";
+            email.Html = "<html><p>Hello</p><p>BAnana</p></html>";
+
+            SMTP server = SMTP.GenerateInstance(new NetworkCredential(username, password, "apphb.com"));
+            server.Deliver(email);
+
             return View();
         }
     }
