906b26a08b6c4564ee19eb23ddc0fd22b3c64e8d
Chelsea Youmans <chelsea@oochelz.com>
2012-08-07 22:33:08 -0400

    Adding previous button

--- Projexi/Projexi/Scripts/ProjectSubmission.js
+++ Projexi/Projexi/Scripts/ProjectSubmission.js
@@ -1,6 +1,7 @@
 ﻿(function ($) {
     $(document).ready(function () {
         $('#overview-submit').click(OnContinueClick);
+        $('#details-previous').click(OnPreviousClick);
     });
 
     function OnContinueClick(evt) {
@@ -8,5 +9,12 @@
         $('.overview').hide('slide', { direction: 'left' }, 1000);
         $('.details').show('slide', { direction: 'right' }, 1000);
         $('#subheader').text('Details');
+    }
+
+    function OnPreviousClick(evt) {
+        evt.preventDefault();
+        $('.details').hide('slide', { direction: 'right' }, 1000);
+        $('.overview').show('slide', { direction: 'left' }, 1000);
+        $('#subheader').text('Overview');
     }
 })(jQuery);
